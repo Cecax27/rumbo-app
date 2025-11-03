885e45ee068e9e8322229b40e84eaabb10161add
Carlos Cervantes <carlos27.10.98@gmail.com>
2025-11-03 12:02:42 -0600

    feat: add constants for account types, transaction categories, colors, and icons

---
+++ shared/constants/icons.js
@@ -0,0 +1,12 @@
+export const ACCOUNT_ICONS = [
+                                { name: 'credit-card', label: 'Credit Card' },
+                                { name: 'card-giftcard', label: 'Gift Card' },
+                                { name: 'account-balance-wallet', label: 'Cash' },
+                                { name: 'account-balance', label: 'Bank Account' },
+                                { name: 'monetization-on', label: 'Investment' },
+                                { name: 'money-off', label: 'Loan' },
+                                { name: 'attach-money', label: 'Money' },
+                                { name: 'money', label: 'Currency' },
+                                { name: 'savings', label: 'Savings' },
+                                { name: 'account-tree', label: 'Investment' }
+                            ]
